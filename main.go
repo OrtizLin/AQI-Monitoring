6f455b3e6d3bbf2865e9dfe6203707f204ad5dfb
OrtizLin <chenghsien852@gmail.com>
2018-04-03 10:06:45 +0800

    Add distance package

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"distancepackage"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	dis, err := strconv.ParseFloat("10000000", 64)
+	if err != nil {
+	}
+	site := ""
+
 	resp, err := http.Get("http://opendata2.epa.gov.tw/AQI.json")
 	if err != nil {
 		log.Fatal(err)
@@ -19,6 +26,19 @@ func main() {
 	}
 	for i := 0; i < len(generic); i++ {
 		md := generic[i].(map[string]interface{})
+		lat1, err := strconv.ParseFloat(md["Latitude"].(string), 64)
+		lon1, err := strconv.ParseFloat(md["Longitude"].(string), 64)
+		lat2, err := strconv.ParseFloat("25.048220", 64)  //User's Lat
+		lon2, err := strconv.ParseFloat("121.532731", 64) //User's Long
+		if err != nil {
+
+		}
 		fmt.Println(md["SiteName"], md["AQI"], md["Status"], md["Latitude"], md["Longitude"])
+		rangeOfTwoPoint := distancepackage.Distance(lat1, lon1, lat2, lon2)
+		if rangeOfTwoPoint < dis {
+			dis = rangeOfTwoPoint
+			site = md["SiteName"].(string)
+		}
 	}
+	fmt.Println("The shortest site is :", site, "range is :", dis)
 }
