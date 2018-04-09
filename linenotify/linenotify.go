d85b539443e94666cd42e56fb1eee485d5b5011c
OrtizLin <chenghsien852@gmail.com>
2018-04-09 11:27:18 +0800

    token

--- linenotify/linenotify.go
+++ linenotify/linenotify.go
@@ -60,7 +60,7 @@ func Token(w http.ResponseWriter, req *http.Request) {
 	}
 	defer session.Close()
 	collect := session.DB("aqidb").C("userdb")
-	loc := []string{"123", "456", "000"}
+	loc := []string{}
 	user := User{}
 	user.UserToken = accessToken
 	user.UserClientID = param1
