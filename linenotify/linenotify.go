11b7192645d6dda7fac8f90d7922e35eae46ef7a
OrtizLin <chenghsien852@gmail.com>
2018-04-09 10:22:03 +0800

    token test

--- linenotify/linenotify.go
+++ linenotify/linenotify.go
@@ -11,7 +11,8 @@ import (
 )
 
 func Auth(w http.ResponseWriter, req *http.Request) {
-	c, err := auth.New(os.Getenv("ClientID"), os.Getenv("APP_BASE_URL")+"pushnotify")
+	param1 := req.URL.Query().Get("client")
+	c, err := auth.New(os.Getenv("ClientID"), os.Getenv("APP_BASE_URL")+"pushnotify?client="+param1)
 	if err != nil {
 		fmt.Fprintf(w, "error:%v", err)
 		return
@@ -21,6 +22,7 @@ func Auth(w http.ResponseWriter, req *http.Request) {
 }
 
 func Token(w http.ResponseWriter, req *http.Request) {
+	param1 := req.URL.Query().Get("client")
 	resp, err := auth.ParseRequest(req)
 	if err != nil {
 		fmt.Fprintf(w, "error:%v", err)
@@ -43,5 +45,5 @@ func Token(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "error:%v", err)
 		return
 	}
-	fmt.Println(accessToken)
+	fmt.Println(accessToken, "OOOOO", param1)
 }
